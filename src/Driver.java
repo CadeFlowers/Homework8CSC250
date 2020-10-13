5810bb352b6ccbbea7dad9ed7bdeddab9093c8b9
CadeFlowers <70229600+CadeFlowers@users.noreply.github.com>
2020-10-13 09:49:29 -0500

    Hw7 Starting Point

--- src/Driver.java
+++ src/Driver.java
@@ -1,9 +1,33 @@
+import java.io.File;
+import java.util.Scanner;
+import java.util.ArrayList;
 
-public class Driver {
-
-	public static void main(String[] args) 
+public class Driver 
+{
+	public static void main(String[] args) throws Exception
 	{
-		System.out.println("hello");
+		ArrayList<BibleBook> theBooks = new ArrayList<BibleBook>();
+		Scanner input = new Scanner(new File(System.getProperty("user.dir") + "/src/data.dat"));
+		while(input.hasNext()) 
+		{
+			theBooks.add(new BibleBook(input.nextLine()));
+		}
+		input.close();
+		
+		Driver.searchSummaries(theBooks);
+	} 
+	static void searchSummaries(ArrayList<BibleBook> theBooks) 
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a serach term: ");
+		String term = input.nextLine();
+		for(int i = 0; i < theBooks.size(); i++) 
+		{
+			if(theBooks.get(i).searchSummary(term)) 
+			{
+				System.out.println(theBooks.get(i).getName());
+				break;
+			}
+		}
 	}
-
 }
